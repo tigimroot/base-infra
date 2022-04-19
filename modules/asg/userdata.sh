4d9fcac7b5d8e0e30fafc8e7c1735adedaed686a
AK <tigimroot@gmail.com>
2022-04-19 08:10:46 +0400

    edited userdata.sh

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -6,6 +6,6 @@ myip=`curl http://checkip.amazonaws.com`
 sudo usermod -a -G apache ec2-user
 sudo chown -R ec2-user:apache /var/www
 sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
-sudo echo "Octarine server with IP: <h2>$myip</h2><br>" > /var/www/html/index.html
+sudo echo "KW server with IP: <h2>$myip</h2><br>" > /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
